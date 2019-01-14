0f2addfd114b75e00d541292d9302b090c4c8f37
Barış İşbiliroğlu <44321535+Bisbiliroglu@users.noreply.github.com>
2019-01-14 22:54:40 +0300

    Update LM35.ino

--- LM35/LM35.ino
+++ LM35/LM35.ino
@@ -1,6 +1,6 @@
 #include <LiquidCrystal.h>
 
-const int lm35pin = 1, buttonpin = 0;
+const int lm35pin1 = 1, lm35pin2 = 2, buttonpin = 0;
 float eski_sicaklik = 0;
 int button_value = 0, analog_value = 0;
 const int rs = 8, en = 9, d4 = 4, d5 = 5, d6 = 6, d7 = 7, backlight = 10;
@@ -13,7 +13,7 @@ float float_map(long x, long in_min, long in_max, long out_min, long out_max){
 }
 
 
-float readLM35_1(void){
+float readLM35_1(int lm35pin){
   // default 5V analog referes içöin kullanılabilir.
   float temp_value = float_map(analogRead(lm35pin), 0, 1023, 0, 500 );
   for(int i = 0; i<99; i++){
@@ -25,7 +25,7 @@ float readLM35_1(void){
   return ((temp_value/100.00) + eski_sicaklik)/2;
 }
 
-float readLM35_2(void){
+float readLM35_2(int lm35pin){
   //internal 1V1 analog referans için kullanılabilir. 
   float temp_value = float_map(analogRead(lm35pin), 0, 1023, 0, 110 );
   for(int i = 0; i<99; i++){
@@ -48,9 +48,10 @@ void setup() {
 void loop() {
    //lcd.clear();
    lcd.setCursor(0,0);
-   lcd.print("Sicaklik:"+(String)readLM35_2()+(char)223+"C");
+   lcd.print("Beyaz : "+(String)readLM35_2(lm35pin1)+(char)223+"C");
    lcd.setCursor(0,1);
-   lcd.print("Analog  :"+(String)analog_value+"  ");
+   //lcd.print("Analog  :"+(String)analog_value+"  ");
+   lcd.print("Siyah : "+(String)readLM35_2(lm35pin2)+(char)223+"C");
    delay(500);
 
    /*button_value = analogRead(buttonpin);
